21def5e24e5ceb3274d66d0a9fbc9afd3910e556
vitorcontiz <vitorcontizgit@gmail.com>
2024-04-08 22:52:32 -0300

    test: ensure AuthMiddleware calls LoadACcountByToken with correct token

--- src/data/usecases/list-transactions/db-list-transactions.spec.ts
+++ src/data/usecases/list-transactions/db-list-transactions.spec.ts
@@ -1,4 +1,4 @@
-import { TransactionModel } from '../../../domain/usecases/list-transaction'
+import { TransactionModel, TransactionStatus } from '../../../domain/usecases/list-transaction'
 import { ListTransactionsRepository } from '../../protocols/db/list-transactions/list-transactions-repository'
 import DbListTransactions from './db-list-transactions'
 
@@ -6,10 +6,23 @@ type SutTypes = {
   sut: DbListTransactions
   listTransactionsRepositoryStub: ListTransactionsRepository
 }
+const makeFakeTransactions = () => {
+  return [
+    {
+      transactionId: "123456789",
+      accountId: "987654321",
+      amount: 100.50,
+      timestamp: new Date('2024-04-08'),
+      description: "Payment for groceries",
+      category: "Food",
+      status: TransactionStatus.Completed
+    }
+  ]
+}
 const makeListTransactionsRepository = (): ListTransactionsRepository => {
   class ListTransactionsRepositoryStub implements ListTransactionsRepository {
     async loadAll (): Promise<TransactionModel[]> {
-      return new Promise(resolve => resolve([]))
+      return new Promise(resolve => resolve(makeFakeTransactions()))
     }
   }
   return new ListTransactionsRepositoryStub()
@@ -24,11 +37,17 @@ const makeSut = (): SutTypes => {
   }
 }
 
+
 describe('DbListTransactions', () => {
   test('Should call ListTransactionsRepository', async () => {
     const { sut, listTransactionsRepositoryStub } = makeSut()
     const listSpy = jest.spyOn(listTransactionsRepositoryStub, 'loadAll')
     await sut.load()
     expect(listSpy).toHaveBeenCalled()
+  })
+  test('Should return a ListTransactionsModel on success', async () => {
+    const { sut } = makeSut()
+    const transactions = await sut.load()
+    expect(transactions).toEqual(makeFakeTransactions())
   })
 })
